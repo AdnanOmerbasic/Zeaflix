2e22a69972daa1e2bb9f21ba47de8917bc1a2850
AdnanOmerbasic <AdnanOmerbasic99@gmail.com>
2024-04-11 04:13:18 +0200

    setting up store and api

---
+++ src/store/apis/moviesApi.js
@@ -0,0 +1,27 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const moviesApi = createApi({
+  reducerPath: "movies",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://api.themoviedb.org/3/",
+  }),
+  endpoints(builder) {
+    return {
+      fetchPopularMovies: builder.query({
+        query: () => {
+          return {
+            url: "discover/movie",
+            params: {
+              sort_by: "popularity.desc",
+              api_key: "3867a8eaac8a5d3191ed5ec4b9b38f95",
+            },
+            method: "GET",
+          };
+        },
+      }),
+    };
+  },
+});
+
+export const { useFetchPopularMoviesQuery } = moviesApi;
+export { moviesApi };
